69b2e851e844990bf51ce03194e2369301215c26
Karan Shetty <shettykaran2110@gmail.com>
2021-04-22 13:46:45 +0530

    Header and footer in layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+
 import styles from '../styles/Layout.module.css';
+import Footer from './Footer';
+import Header from './Header';
 
 const Layout = ({ title, keywords, description, children }) => {
   return (
@@ -9,7 +12,10 @@ const Layout = ({ title, keywords, description, children }) => {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
+
+      <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 };
