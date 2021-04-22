00339d9c0d59c49d3353c7c0d794b2c82edca02a
Karan Shetty <shettykaran2110@gmail.com>
2021-04-22 12:47:02 +0530

    Pages and routing

---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+const AddEvent = () => {
+  return (
+    <div>
+      <h1>Add Event</h1>
+    </div>
+  );
+};
+
+export default AddEvent;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+const EventsPage = () => {
+  return (
+    <div>
+      <h1>My events</h1>
+    </div>
+  );
+};
+
+export default EventsPage;
